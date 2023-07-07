9f8957533c3c9ea3549ca996c66240c773328c5e
FennBoii <Thepleasantplant@gmail.com>
2023-07-07 02:53:55 -0700

    updated main.js
    simple fix that keeps ("closing in ##") open with no way to close it than to restart client

--- src/main.js
+++ src/main.js
@@ -1114,6 +1114,7 @@ function setActivity() {
 	}
 
 	if (title && CountdownTimerVar === false) {
+		warningText = '';
 		if (repeat.includes('off')) {
 			var ThirdEntry = '▶️ •';
 			qualities += 1;

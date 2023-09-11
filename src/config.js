de70e78765f2f3642188ab2c4dd85b5f1adec230
FennBoii <Thepleasantplant@gmail.com>
2023-09-10 21:22:01 -0700

    added config File
    added a config file to make it easier to make changes to things and make things work

---
+++ src/config.js
@@ -0,0 +1,11 @@
+    module.exports = {
+        "username": "[ Welcome Back, Fennec ]",
+        "SiteName": "ccccc",
+        "GivenNameToken": "aed4ef3b90d74390e125f08b74912a65b3760869",
+        "RandomToken": "3Z1Rtvfwir",
+        // delays between sends (used for slower connections and better syncing)
+        "outOfSyncPlayingSong": 3,
+        // (this is for if the song is out of sync by; how much will you want it resync again?
+        "resyncSongUrl": 6000
+        // (this is for how often you want to resync the url of the current song, I recommend nothing below 6000 because if it's only partially loaded, it will reload the page again and you'll be stuck in a loop
+    }

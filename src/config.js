b78ab3a00ddfd3109f293b64f6a7c975cbd75629
FennBoii <Thepleasantplant@gmail.com>
2023-09-12 22:34:09 -0700

    updated 09/12/2023

--- src/config.js
+++ src/config.js
@@ -1,12 +1,8 @@
     module.exports = {
-        "SiteName": "FennBoii",
-        "GivenNameToken": "4787bb1adad3530f01f72869356668b294284a83",
-        "RandomToken": "A4ngKTS8A3",
-        "SVLPath": "./svcl.exe",
-        "username": "FennBoiics",
-        // delays between sends (used for slower connections and better syncing)
+        "SiteName": "",
+        "GivenNameToken": "",
+        "RandomToken": "",
+        "username": "",
         "outOfSyncPlayingSong": 3,
-        // (this is for if the song is out of sync by; how much will you want it resync again?
         "resyncSongUrl": 6000
-        // (this is for how often you want to resync the url of the current song, I recommend nothing below 6000 because if it's only partially loaded, it will reload the page again and you'll be stuck in a loop
     }
